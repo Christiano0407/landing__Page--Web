8e0d7cfdc498ed8f93661959767cbf7843df095f
Christiano0407 <placebo0704@gmail.com>
2022-02-02 12:30:36 -0600

    gsap

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
 console.group("Landing Page Web");
 
+// GSAP =====
+let tween = gsap.from(
+  ".navigation",
+  {
+    delay: 1.5,
+    opacity: 0,
+    duration: 1,
+    y: "-50",
+    ease: Expo.easeInOut,
+  },
+  0.0
+);
+
+// Swipper ===
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 30,
