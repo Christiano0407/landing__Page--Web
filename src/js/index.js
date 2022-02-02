e31c9ab94d727dd3f1fe3ec610ef916ac87effe6
Christiano0407 <placebo0704@gmail.com>
2022-02-02 09:33:09 -0600

    previous

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 console.group("Landing Page Web");
 
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 console.groupEnd();
